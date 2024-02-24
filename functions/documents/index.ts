63cb2c34f92a0a83232f9b4688d0d72de9d3e973
DeeplyAdore <47216867+DeeplyAdore@users.noreply.github.com>
2024-02-24 02:32:52 -0600

    Update index.ts

--- functions/documents/index.ts
+++ functions/documents/index.ts
@@ -26,7 +26,7 @@ import { Environment, HTTPError } from "../..";
 
 function generateId(size: number): string {
   let id = "";
-  const keyspace = "ABCDEFGHIJKLMNPQRSTUVWXYZ123456789";
+  const keyspace = "ABCDEFGHJKLMNPQRSTUVWXYZ123456789";
 
   for (let idx = 0; idx < size; idx++) {
     id += keyspace.charAt(Math.random() * keyspace.length);
