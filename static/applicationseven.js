0709947925383d034e4a669ac3b34d7aea1edbf5
DeeplyAdore <47216867+DeeplyAdore@users.noreply.github.com>
2024-04-30 05:22:42 -0500

    Add files via upload

--- static/applicationseven.js
+++ static/applicationseven.js
@@ -92,7 +92,7 @@ document.querySelector('#firstPromptText').innerText = "Pick jewelry to wear out
 document.querySelector('#pic1').src = "Xi.webp";
 document.querySelector('#pic2').src = "Rx.webp";
 document.querySelector('#pic3').src = "Ff.webp";
-document.querySelector('#pic4').src = "Xi.webp";
+document.querySelector('#pic4').src = "Cw.webp";
 document.querySelector('#pic5').src = "Mm.webp";
 document.querySelector('#pic6').src = "Eg.webp";
 
