e002cf6029e11bd1650654b3c3d9a25aaf917f83
DeeplyAdore <47216867+DeeplyAdore@users.noreply.github.com>
2024-04-30 05:00:00 -0500

    Add files via upload

--- static/applicationseven.js
+++ static/applicationseven.js
@@ -93,7 +93,7 @@ document.querySelector('#pic1').src = "Xi.webp";
 document.querySelector('#pic2').src = "Rx.webp";
 document.querySelector('#pic3').src = "Ff.webp";
 document.querySelector('#pic4').src = "Xi.webp";
-document.querySelector('#pic5').src = "Xi.webp";
+document.querySelector('#pic5').src = "Mm.webp";
 document.querySelector('#pic6').src = "Xi.webp";
 
 document.querySelector('#pa1').id = "pb1";
