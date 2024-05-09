f0173b52dd06f425d6a43f4474375489dc8ed4eb
DeeplyAdore <47216867+DeeplyAdore@users.noreply.github.com>
2024-05-09 02:50:38 -0500

    Add files via upload

--- static/applicationseven.js
+++ static/applicationseven.js
@@ -603,7 +603,7 @@ document.title = "Now Pick Shoes";
 skipToTwo();
 break;
 case "p5":
-window.open("https://www.kqzyfj.com/click-101050243-13920803?url=https%3A%2F%2Ftheluxurycloset.com%2Fwomen%2Fprada-pink-mini-terry-spugna-re-edition-2000-bag-p974253", '_blank');
+window.open("https://www.jdoqocy.com/click-101050243-13920803?url=https%3A%2F%2Ftheluxurycloset.com%2Fwomen%2Fhermes-cyclamen-chevre-leather-palladium-finish-birkin-30-bag-p927453", '_blank');
 document.title = "Now Pick Shoes";
 skipToTwo();
 break;
