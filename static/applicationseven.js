9c5cd0cbb60caae8760ce617e6e824d5d313ae19
DeeplyAdore <47216867+DeeplyAdore@users.noreply.github.com>
2024-05-10 03:39:00 -0500

    Add files via upload

--- static/applicationseven.js
+++ static/applicationseven.js
@@ -95,7 +95,7 @@ document.querySelector('#firstPromptText').innerText = "Pick jewelry to wear to 
 document.querySelector('#pic1').src = "Xo.webp";
 document.querySelector('#pic2').src = "Ww.webp";
 document.querySelector('#pic3').src = "Ff.webp";
-document.querySelector('#pic4').src = "Cj.webp";
+document.querySelector('#pic4').src = "Xs.webp";
 document.querySelector('#pic5').src = "Mm.webp";
 document.querySelector('#pic6').src = "Ss.webp";
 
@@ -730,7 +730,7 @@ document.title = "Finally Pick Your Gift";
 skipToFour();
 break;
 case "pb4":
-win2 = window.open("https://www.anrdoezrs.net/click-101050243-13920803?url=https%3A%2F%2Ftheluxurycloset.com%2FWomen%2Fcartier-18k-yellow-gold-love-bangle-bracelet-p974540", '_blank');
+win2 = window.open("https://www.tkqlhce.com/click-101050243-13920803?url=https%3A%2F%2Ftheluxurycloset.com%2Fmen%2Fcartier-white-22-stainless-steel-panthere-de-cartier-w4pn0007-p904075", '_blank');
 document.title = "Finally Pick Your Gift";
 skipToFour();
 break;
